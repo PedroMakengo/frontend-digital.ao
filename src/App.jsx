67248596e057cfd6d626b76fb9b1e86bcdc258d3
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-01-19 13:22:54 -0300

    Melhorando o padrão de arquivos do meu projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const  App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,4 +20,4 @@ function App() {
   );
 }
 
-export default App;
+export { App };
