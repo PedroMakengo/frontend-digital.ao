43317445f3563139ee8c406809fc50fc6980b99d
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-01-26 12:22:29 -0300

    Trabalhando

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,12 +2,19 @@ import React from "react";
 
 import { FooterStyle } from "./style.js";
 
+import Logo from "../../assets/images/logo.png";
+
 const Footer = () => {
   return (
     <FooterStyle>
       <div className="container">
         <div className="itemFooter">
-          <h2>Barcelona</h2>
+          <img src={Logo} alt="Digital.ao" />
+          <p>
+            Dizer sobre a inovação da digital.ao a inovação da digital.aoa
+            inovação da digital.aoa inovação da digital.aoa inovação da
+            digital.aoa inovação da digital.ao
+          </p>
         </div>
       </div>
     </FooterStyle>
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,13 +1,26 @@
 import styled from "styled-components";
 
 export const FooterStyle = styled.div`
-  height: 50vh;
+  height: 20vh;
   background: #000;
+
+  padding: 50px 0;
 
   .container {
     max-width: 1100px;
     margin: 0 auto;
 
     color: #fff;
+    text-align: center;
+
+    img {
+      width: 15%;
+    }
+    p {
+      margin: 0 auto;
+      width: 600px;
+
+      margin-top: 1rem;
+    }
   }
 `;
